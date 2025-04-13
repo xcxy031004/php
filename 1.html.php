2e1c024a988cfd77a8e8105e8b4bf93d415f3aa8
xcxy031004 <xavierchew31@gmail.com>
2025-04-13 23:35:10 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,12 +1,12 @@
 <head>
 </head>      
 <body> 
-    <h1>My First PHP </h1>#page title
+    <h1>My First PHP </h1>#頁面標題
     <h2>
-     <h1>My First PHP </h1>#page title
+     <h1>My First PHP </h1>#頁面標題
     <h2> 
      <?php
-         echo "Hello World!!!";#print output
+         echo "hello world!!!";#輸出hello world！！！
       ?>
       </h2>
 </body>

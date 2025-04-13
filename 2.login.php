be235e32cb53ec56513480cc944de8f3ed110402
xcxy031004 <xavierchew31@gmail.com>
2025-04-14 01:20:17 +0800

    Update 2.login.php

--- 2.login.php
+++ 2.login.php
@@ -1,5 +1,5 @@
 <?php 
-    echo $_POST["id"]; #get username in browser
-    echo "<br>";
-    echo $_POST["pwd"]; #get pwd in browser
+    echo $_POST["id"]; #將帳號（id）的值輸出到瀏覽器
+    echo "<br>";#插入 HTML 的換行標籤，讓帳號和密碼分開顯示在不同的行
+    echo $_POST["pwd"]; #將密碼的值輸出到瀏覽器
 ?>

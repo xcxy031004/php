7fbcf4efd4f895431b2b12989a2fae4225919cb8
xcxy031004 <xavierchew31@gmail.com>
2025-04-14 01:22:19 +0800

    Update 3.login.php

--- 3.login.php
+++ 3.login.php
@@ -1,6 +1,6 @@
 <?php 
-    if (($_POST["id"] == "xcxy031004") && ($_POST["pwd"]=="xcxy031004"))  #set username and pwd 
-        echo"Login successful";    #will display this if true
+    if (($_POST["id"] == "xcxy031004") && ($_POST["pwd"]=="xcxy031004"))  #設定賬密
+        echo"Login successful";#如果條件成立，表示使用者輸入的帳號和密碼正確，輸出「Login successful」到瀏覽器。
     else
-        echo "Login failed";    #will display this if false
+        echo "Login failed";#如果條件不成立，表示使用者輸入的帳號或密碼不正確，輸出「Login failed」到瀏覽器。
 ?>

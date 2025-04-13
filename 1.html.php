3ef4826ffd3cafac4fcd991792e92818e669eb24
xcxy031004 <xavierchew31@gmail.com>
2025-04-13 23:08:46 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,13 @@
+<head>
+</head>      
+<body> 
+    <h1>My First PHP </h1>
+    <h2>
+     <h1>My First PHP </h1>
+    <h2> 
+     <?php
+         echo "Hello World";
+         echo "Hello World";
+      ?>
+      </h2>
+</body>

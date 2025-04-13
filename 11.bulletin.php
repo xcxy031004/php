7986ece300d2293fa74d91ca396435902effee20
xcxy031004 <xavierchew31@gmail.com>
2025-04-13 23:22:37 +0800

    Create 11.bulletin.php

---
+++ 11.bulletin.php
@@ -0,0 +1,32 @@
+#•Check if the user is logged in.
+#•If so, display the bulletin data table and bulletin management features (add, edit, delete).
+#•If not logged in, redirect to the login page.
+<?php
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "請登入";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{
+        echo "歡迎, ".$_SESSION["id"]."[<a href=12.logout.php>登出</a>] [<a href=18.user.php>管理使用者</a>] [<a href=22.bulletin_add_form.php>新增佈告</a>]<br>";
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+        $result=mysqli_query($conn, "select * from bulletin");
+        echo "<table border=2><tr><td>Notice No</td><td>Notice Category</td><td>title</td><td>Announcement content</td><td>Release time</td></tr>";
+        while ($row=mysqli_fetch_array($result)){
+            echo "<tr><td><a href=26.bulletin_edit_form.php?bid={$row["bid"]}>修改</a> 
+            <a href=28.bulletin_delete.php?bid={$row["bid"]}>刪除</a></td><td>";
+            echo $row["bid"];
+            echo "</td><td>";
+            echo $row["type"];
+            echo "</td><td>"; 
+            echo $row["title"];
+            echo "</td><td>";
+            echo $row["content"]; 
+            echo "</td><td>";
+            echo $row["time"];
+            echo "</td></tr>";
+        }
+        echo "</table>";
+    }
+?>

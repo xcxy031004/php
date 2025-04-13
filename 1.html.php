2f62029796cbbb6e24f30e3e2099d669ae4b9663
xcxy031004 <xavierchew31@gmail.com>
2025-04-13 23:18:52 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,13 +1,12 @@
 <head>
 </head>      
 <body> 
-    <h1>My First PHP </h1>
+    <h1>My First PHP </h1>#page title
     <h2>
-     <h1>My First PHP </h1>
+     <h1>My First PHP </h1>#page title
     <h2> 
      <?php
-         echo "Hello World";
-         echo "Hello World";
+         echo "Hello World!!!";#print output
       ?>
       </h2>
 </body>

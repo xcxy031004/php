3d466eaa9685215c6b6ab51686a1bbc44cf86bd1
xcxy031004 <xavierchew31@gmail.com>
2025-04-13 23:25:38 +0800

    Create 3.login.php

---
+++ 3.login.php
@@ -0,0 +1,6 @@
+<?php 
+    if (($_POST["id"] == "xcxy031004") && ($_POST["pwd"]=="xcxy031004"))  #set username and pwd 
+        echo"Login successful";    #will display this if true
+    else
+        echo "Login failed";    #will display this if false
+?>

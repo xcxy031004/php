df519ca7a13fcbbf499bce29e0b36a913eb8db3f
xcxy031004 <xavierchew31@gmail.com>
2025-04-14 00:14:14 +0800

    Update 12.logout.php

--- 12.logout.php
+++ 12.logout.php
@@ -1,7 +1,7 @@
 #delete the login session and redirect the user back to the login page
 <?php
-    session_start();
-    unset($_SESSION["id"]);
-    echo "Logout successful....";
+    session_start();#啟動會話
+    unset($_SESSION["id"]);#刪除使用者 ID
+    echo "Logout successful....";#顯示登出成功訊息
     echo "<meta http-equiv=REFRESH content='3; url=2.login.html'>";
 ?>

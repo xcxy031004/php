52477a756dbaec60cbd90c075e60db8fcac63a86
xcxy031004 <xavierchew31@gmail.com>
2025-04-14 09:20:05 +0800

    Update 7.bulletin.php

--- 7.bulletin.php
+++ 7.bulletin.php
@@ -5,7 +5,7 @@
     $result=mysqli_query($conn, "select * from bulletin");
     echo "<table border=2><tr><td>Notice No</td><td>Notice Category</td><td>title</td><td>Announcement content</td><td>Release time</td></tr>";
     while ($row=mysqli_fetch_array($result)){
-        echo "<tr><td>";
+        echo "<tr><td>";#每行的 id、type、title、content 和 time 資料分別填入 HTML 表格中，並使用 <td> 標籤顯示。
         echo $row["bid"];
         echo "</td><td>";
         echo $row["type"];
@@ -15,7 +15,7 @@
         echo $row["content"]; 
         echo "</td><td>";
         echo $row["time"];
-        echo "</td></tr>";
+        echo "</td></tr>";#每行資料均包裹在 <tr> 標籤中，形成表格的每一列。
     }
     echo "</table>"
 ?>

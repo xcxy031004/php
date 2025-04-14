dafa5bfa92f449fdfd3371ad72fd3283141cbd8d
xcxy031004 <xavierchew31@gmail.com>
2025-04-14 09:16:57 +0800

    Update 6.login.php

--- 6.login.php
+++ 6.login.php
@@ -1,18 +1,16 @@
 <?php
    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
    $result=mysqli_query($conn, "select * from user");
-   $login=FALSE;
+   $login=FALSE;#比較使用者提交的帳號（$_POST["id"]）和密碼（$_POST["pwd"]）是否與每一行的 id 和 pwd 相符。
    while ($row=mysqli_fetch_array($result)) {
      if (($_POST["id"]==$row["id"]) && ($_POST["pwd"]==$row["pwd"])) {
-       $login=TRUE;
+       $login=TRUE;#如果找到匹配的記錄，將 $login 變數設為 TRUE，表示登入成功。
      }
    } 
-#If login is successful: display a message and redirect to bulletin.php after 3 seconds
    if ($login==TRUE){echo "Login successful";
     echo "<meta http-equiv=REFRESH content='3,url=bulletin.php'>";
   }
   else{
-#if login fails: display error message and still redirect to bulletin.php after 3 seconds
     echo "Account/Password Error";
     echo "<meta http-equiv=REFRESH content='3,url=bulletin.php'>";
   }

cca88058be4e0c0e126a22ddfa17d1b7153a4f3d
xcxy031004 <xavierchew31@gmail.com>
2025-06-06 14:06:31 +0800

    Create 26.bulletin_edit_form.php

---
+++ 26.bulletin_edit_form.php
@@ -0,0 +1,41 @@
+<?php
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "please login first";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{
+        #這行程式碼透過 SELECT 指令，取得 bulletin 資料表中符合 bid=$_GET["bid"] 的公告資訊，並存入 $row。
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+        $result=mysqli_query($conn, "select * from bulletin where bid={$_GET["bid"]}");
+        $row=mysqli_fetch_array($result);
+        $checked1="";
+        $checked2="";
+        $checked3="";
+        if ($row['type']==1)
+            $checked1="checked";
+        if ($row['type']==2)
+            $checked2="checked";
+        if ($row['type']==3)
+            $checked3="checked";#用來決定公告類型 (type) 是否預先被選中，確保載入公告時呈現正確的類型。
+      #輸出 HTML 表單
+        echo "
+        <html>
+            <head><title>新增佈告</title></head>
+            <body>
+                <form method=post action=27.bulletin_edit.php>
+                    佈告編號：{$row['bid']}<input type=hidden name=bid value={$row['bid']}><br>
+                    標    題：<input type=text name=title value={$row['title']}><br>
+                    內    容：<br><textarea name=content rows=20 cols=20>{$row['content']}</textarea><br>
+                    佈告類型：<input type=radio name=type value=1 {$checked1}>系上公告 
+                            <input type=radio name=type value=2 {$checked2}>獲獎資訊
+                            <input type=radio name=type value=3 {$checked3}>徵才資訊<br>
+                    發布時間：<input type=date name=time value={$row['time']}><p></p>
+                    <input type=submit value=修改佈告> <input type=reset value=清除>
+                </form>
+            </body>
+        </html>
+        ";
+    }
+?>
